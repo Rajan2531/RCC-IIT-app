84a6636fd3f0410375d00d79c928c54b054df5df
rajan chouhan <rajan.chouhan25@gmail.com>
2023-05-24 10:05:47 +0530

    removed protected route

--- routers/courseRouter.js
+++ routers/courseRouter.js
@@ -2,6 +2,6 @@ const express=require("express");
 const router=express.Router();
 const authController=require('./../controllers/authController.js');
 const courseController=require("./../controllers/courseController.js");
-router.route('/').get(authController.protect,courseController.getAllcourses).post(authController.protect,courseController.createCourses);
+router.route('/').get(courseController.getAllcourses).post(courseController.createCourses);
 
 module.exports=router;
